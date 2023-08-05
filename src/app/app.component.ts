288fed65c3575590487fb90d60f4eeff170ce31b
Liam Wedge <21100218@virtualwindow.co.za>
2023-08-05 12:14:57 +0200

    remove animations and further updates to ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component, HostBinding } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';
 import { RouterOutlet } from '@angular/router';
-import { fader } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    fader
-  ]
+
 })
 export class AppComponent {
   title = 'Block-Babies';
 
-  prepareRoute(outlet: RouterOutlet){
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation']
-  }
 }
